5a640ef281248389afd96468feec1e65a1df13b7
Andrew Janke <andrew@apjanke.net>
2021-09-11 12:33:48 -0400

    Refactor: mostly var renaming

--- Mcode/+janklab/+exportmlx/+internal/mergeSameEnvironments.m
+++ Mcode/+janklab/+exportmlx/+internal/mergeSameEnvironments.m
@@ -1,34 +1,44 @@
-function newstr = mergeSameEnvironments(str, environment)
+function out = mergeSameEnvironments(str, environment)
 % Merge same environments
+%
+% out = mergeSameEnvironments(str, environment)
+%
+% Str is a possibly-nonscalar string array, and how it's broken up matters.
+%
+% Returns a possibly-nonscalar string array.
+arguments
+    str string
+    environment (1,1) string
+end
 
 LF = newline;
 
-% str is a vector of string
-% start index of a enviroment
-idx_start = startsWith(str, "\begin{"+environment+"}");
-% start index of a enviroment
-idx_end = endsWith(str, "\end{"+environment+"}");
-
+% Find each occurence of the environment
 % for each \being and \end pair needs to be in one string
-start_lstlisting = find(idx_start);
-end_lstlisting = find(idx_end);
+tfStart = startsWith(str, "\begin{"+environment+"}");
+tfEnd = endsWith(str, "\end{"+environment+"}");
+ixStart = find(tfStart);
+ixEnd = find(tfEnd);
 
-% check if the number of \being and that of \end match.
-assert(sum(idx_start) == sum(idx_end), ...
-    "The number of \begin and that of \end does not match for "...
-    + string(environment) + " environment." + LF ...
-    + "Please contact minoue@mathworks.com with the reproduction step.");
+% Ensure number of \begin and \end match
+assert(numel(ixStart) == numel(ixEnd), ...
+    "The number of \begin and \end tags does not match for " ...
+    + "environment '" + environment + "'." + LF ...
+    + "This is a bug in ExportMlx (or maybe Matlab). " ...
+    + "Please report it at https://github.com/janklab/ExportMlx/issues.");
 
-for ii=1:length(start_lstlisting)
-    idx = start_lstlisting(ii):end_lstlisting(ii);
-    if length(idx) > 1 % skip if \begin and \end are in the same string
+% Do merging
+for i = 1:numel(ixStart)
+    ix = ixStart(i):ixEnd(i);
+    if length(ix) > 1 % skip if \begin and \end are in the same string
         % join them with LF in between
-        mergedstring = strjoin(str(idx), string([LF, LF]));
-        str(idx(1)) = mergedstring;
-        str(idx(2:end)) = "";
+        mergedstring = strjoin(str(ix), string([LF LF]));
+        str(ix(1)) = mergedstring;
+        str(ix(2:end)) = "";
     end
 end
 
-% Delete the deleted empty string
+% Delete empty strings
 str(strlength(str) == 0) = [];
-newstr = str;
+
+out = str;
